df9f49f8de39cc527d58319900b8ea68ad3f173b
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-01-08 11:17:55 -0800

    DB Usage!
    Got reading the DB working! Now I need to properly configure things itself.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use rusty_leveldb::{Options, DB};
+use std::io::Result;
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+    let db: DB = DB::open(
+        "./test/world/Chromebook Survival/db",
+        Options {
+            ..Default::default()
+        },
+    )
+    .unwrap();
+    Ok(())
 }
