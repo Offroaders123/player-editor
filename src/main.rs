1ed2752a8e66ec808ce7593e14d9ae3631fc59df
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-01-09 03:07:54 -0800

    CLI Demo
    Demoing this out as a standalone CLI binary, and it works awesome! Now I just need to make it into it's own organized project.

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,36 @@ use std::fs::{create_dir_all, write};
 use std::io::Result;
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    println!("player-editor");
 
     create_dir_all("./test/world/Chromebook Survival/_player")?;
 
     let mut options: Options = Options::default();
     options.create_if_missing = false;
     options.compressor = SnappyCompressor::ID;
+
     let mut db: DB = DB::open("./test/world/Chromebook Survival/db", options)
         .expect("Failed to open the database");
 
     let mut iter: DBIterator = db.new_iter().expect("Failed to create iterator");
     iter.seek_to_first();
 
+    println!("Finding player entries...");
+
     while iter.valid() {
         let (key, value): (String, Vec<u8>) = match iter.next() {
-            None => break,
             Some((key, value)) => (String::from_utf8_lossy(&key).to_string(), value),
+            None => break,
         };
+
         if !key.to_lowercase().contains("player") {
             continue;
         }
-        println!("Key: {:?}\nValue: {:?}\n", key, value);
-        write(
-            format!("./test/world/Chromebook Survival/_player/{key}.nbt"),
-            value,
-        )?;
+
+        println!("{key}");
+
+        let path: String = format!("./test/world/Chromebook Survival/_player/{key}.nbt");
+        write(path, value)?;
     }
 
     Ok(())
