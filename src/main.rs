c7409972ea6d1204730f4c0ef3ad54666890ec3f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-01-09 13:19:39 -0800

    Proper Path Usage
    I'll have to get some help debugging this, but now I think it should properly work on any system, Windows or POSIX. It doesn't do manual path joining anymore.
    
    https://doc.rust-lang.org/rust-by-example/std_misc/path.html
    https://www.reddit.com/r/rust/comments/7mu7q1/is_working_with_paths_always_this_painful/

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,25 @@ use rusty_leveldb::{
 use std::env::args;
 use std::fs::{create_dir_all, write};
 use std::io::Result;
+use std::path::{Path, PathBuf};
 
 fn main() -> Result<()> {
     println!("player-editor");
 
     let args: Vec<String> = args().collect();
 
-    let world_dir: &String = &args
-        .get(1)
-        .expect_exit("Please pass the world folder you'd like to extract from");
-    let player_dir: String = format!("{world_dir}/_player");
-    let db_dir: String = format!("{world_dir}/db");
+    let world_dir: &Path = Path::new(
+        args.get(1)
+            .expect_exit("Please pass the world folder you'd like to extract from"),
+    );
+    let player_dir: PathBuf = world_dir.join("_player");
+    let db_dir: PathBuf = world_dir.join("db");
 
     let mut options: Options = Options::default();
     options.create_if_missing = false;
     options.compressor = SnappyCompressor::ID;
 
-    println!("Opening world {world_dir}\n");
+    println!("Opening world {:?}\n", world_dir);
 
     let mut db: DB = DB::open(db_dir, options).expect_exit("Failed to open database");
 
@@ -48,8 +50,8 @@ fn main() -> Result<()> {
 
         println!("{key}");
 
-        let path: String = format!("{player_dir}/{key}.nbt");
-        write(path, value)?;
+        let player_path: PathBuf = player_dir.join(format!("{key}.nbt"));
+        write(player_path, value)?;
     }
 
     Ok(())
